bbcbca7d74e7a975ebb15e7fcc6349183c738210
Daniel C <brikbusters@gmail.com>
2025-07-25 21:00:42 -0400

    update x86 examples to use semicolon comments, change address input box style

--- examples/x86-hello.asm
+++ examples/x86-hello.asm
@@ -1,11 +1,11 @@
-mov ebx, 0x9000000 /* UART_DR, write here to print characters */
+mov ebx, 0x9000000 ; UART_DR, write here to print characters
 
 mov esi, string
 top:
-	lodsb /* Load byte from [esi] into al, increment esi by 1 */
+	lodsb ; Load byte from [esi] into al, increment esi by 1
 	test al, al
-	jz end /* jump if zero */
-	mov [ebx], al /* Store al into UART_DR */
+	jz end ; jump if zero
+	mov [ebx], al ; Store al into UART_DR
 	jmp top
 end:
 
