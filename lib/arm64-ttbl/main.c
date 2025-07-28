8cad1b20387ecad878cf86482abeeb3cf778804f
Daniel C <brikbusters@gmail.com>
2025-07-28 16:54:45 -0400

    rewrite arm64 ttbl

--- lib/arm64-ttbl/main.c
+++ lib/arm64-ttbl/main.c
@@ -9,6 +9,16 @@ inline static int read_u32(const void *buf, uint32_t *out) {
 	return 4;
 }
 
+struct BlockDescriptor {
+	uint64_t output_address;
+	uint64_t size;
+	uint32_t upper_attributes;
+	uint32_t lower_attributes;
+
+	uint32_t is_combined;
+	uint64_t block;
+};
+
 struct ARMConfig {
 	/// Base physical address of the CPU
 	uint64_t base_address;
@@ -22,6 +32,11 @@ struct ARMConfig {
 	uint32_t buffer_base;
 	void *priv;
 	int (*get_memory)(struct ARMConfig *cfg, uint8_t *buffer, uint64_t offset, uint32_t size);
+
+	// Combined size of consecutive identical blocks that start and end in the same address
+	uint64_t current_combined_size;
+	// Start address of the combined identical blocks
+	uint64_t initial_common_address;
 };
 
 enum GranuleSize {
@@ -30,18 +45,64 @@ enum GranuleSize {
 	GRANULE_64KB,
 };
 
-struct BlockDescriptor {
-	uint32_t upper_attributes;
-	uint32_t lower_attributes;
-	uint64_t output_address;
-};
-
 static enum GranuleSize get_granule_size(struct ARMConfig *cfg) {
 	uint32_t tg0 = (cfg->tcr_elx >> 14) & 0b11;
 	if (tg0 == 0b00) return GRANULE_4KB;
 	if (tg0 == 0b01) return GRANULE_64KB;
 	if (tg0 == 0b10) return GRANULE_16KB;
 	abort();
+}
+
+static int get_oa_size(struct ARMConfig *cfg) {
+	// https://developer.arm.com/documentation/ddi0601/2025-06/AArch64-Registers/TCR-EL3--Translation-Control-Register--EL3-
+	uint32_t ps = (cfg->tcr_elx >> 16) & 0b111;
+	if (ps == 0b000) return 32;
+	if (ps == 0b001) return 36;
+	if (ps == 0b010) return 40;
+	if (ps == 0b011) return 42;
+	if (ps == 0b100) return 44;
+	if (ps == 0b101) return 48;
+	if (ps == 0b110) return 52;
+	abort();
+}
+
+static uint64_t get_oa(struct ARMConfig *cfg, uint64_t block, int level) {
+	// For block descriptor only
+	uint64_t oa_size = get_oa_size(cfg);
+	enum GranuleSize gsize = get_granule_size(cfg);
+
+	// Won't go higher than 47, [50:48] are res0
+	uint64_t mask = (((uint64_t) (47 < 64)) << (47 & 63)) - 1U;
+
+	// TODO: if FEAT_LPA,
+	// Block descriptor bits[15:12] are bits[51:48] of the OA
+
+	uint64_t oa = ((block & mask) >> 12) << 12;
+	return oa;
+}
+
+#define TERRABYTE 0x10000000000ULL
+#define GIGABYTE 0x40000000ULL
+#define MEGABYTE 0x100000ULL
+#define KILOBYTE 0x400ULL
+static uint64_t get_block_size(struct ARMConfig *cfg, int level) {
+	// https://developer.arm.com/documentation/101811/0104/Translation-granule
+	switch (get_granule_size(cfg)) {
+	case GRANULE_4KB:
+		if (level == 0) return GIGABYTE * 512;
+		if (level == 1) return GIGABYTE;
+		if (level == 2) return MEGABYTE * 2;
+		if (level == 3) return KILOBYTE * 4;
+		abort();
+	case GRANULE_16KB:
+		if (level == 0) return TERRABYTE * 128;
+		if (level == 1) return GIGABYTE * 64;
+		if (level == 2) return MEGABYTE * 32;
+		if (level == 3) return KILOBYTE * 16;
+		abort();
+	case GRANULE_64KB:
+		abort();
+	}
 }
 
 int get_memory_file(struct ARMConfig *cfg, uint8_t *buffer, uint64_t offset, uint32_t size) {
@@ -78,12 +139,16 @@ const char *get_indent_lvl(int level) {
 	return "";
 }
 
-int walk_level(struct ARMConfig *cfg, uint64_t of, int level) {
-	unsigned int last_addr = 0;
-	unsigned int last_block_size = 0;
-	// Number of common identical entries in a row
-	unsigned int common_entries = 0;
+//static int print_section(struct ARMConfig *cfg, struct BlockDescriptor *last_block, uint64_t base, unit64_t size) {
+//	
+//}
 
+int walk_level(struct ARMConfig *cfg, uint64_t of, int level, struct BlockDescriptor *last_block) {
+	if (level > 3) abort();
+
+	struct BlockDescriptor dummy_block;
+
+	int n_block = 0;
 	while (1) {
 		// Read the block
 		uint8_t buffer[8];
@@ -91,54 +156,71 @@ int walk_level(struct ARMConfig *cfg, uint64_t of, int level) {
 		uint32_t l, h;
 		of += read_u32(buffer, &l);
 		of += read_u32(buffer + 4, &h);
-		uint64_t block = ((uint64_t)h << 32) & l;
+
+		uint64_t full_block = ((uint64_t)h << 32) | l;
 
 		// Check valid bit
 		if ((l & 1) != 1) {
-			if (common_entries != 0) {
-				printf("%s%d common entries of size 0x%x\n", get_indent_lvl(level), common_entries, last_block_size);
-				common_entries = 0;
+			if (cfg->current_combined_size != 0) {
+				printf("%sCombined blocks of size 0x%lx\n", get_indent_lvl(level), cfg->current_combined_size);
 			}
 
-			printf("%sInvalid block (0x%lx) at 0x%lx\n", get_indent_lvl(level), block, of);
+			printf("%sInvalid block (0x%lx) at 0x%lx\n", get_indent_lvl(level), full_block, of);
 
 			break;
 		}
 
-		// if 1:1 is 0, then we have a block descriptor
-		if ((l & (1 << 1)) == 0) {
-			// temporary hack to compute address in this descriptor
-			uint32_t oa_addr_n = 0xa;
-			uint32_t oa_addr_mask = 0xffffffff;
-			uint32_t addr = l & (oa_addr_mask << oa_addr_n);
-			if (last_addr != 0x0) {
-				uint32_t block_size = addr - last_addr;
-				if (block_size == last_block_size) {
-					// TODO: Check attributes to make sure the blocks are identical
-					common_entries++;
-				} else {
-					if (common_entries != 0) {
-						printf("%s%d common entries of size 0x%x\n", get_indent_lvl(level), common_entries, last_block_size);
-						common_entries = 0;
-					} else {
-						printf("%sBlock descriptor -> 0x%x\n", get_indent_lvl(level), addr);
-						printf("%sDescriptor is at 0x%lx\n", get_indent_lvl(level), of);
-					}
-				}
+		uint64_t output_address = get_oa(cfg, full_block, level);
 
-				last_block_size = block_size;
+		// For lookup levels other than lookup level 3, one of the following:
+		// - If bit[1] is 0, then the descriptor is a Block descriptor.
+		// - If bit[1] is 1, then the descriptor is a Table descriptor.
+		// For lookup level 3, one of the following:
+		// - If bit[1] is 0, then the descriptor is reserved, and treated as invalid.
+		// - If bit[1] is 1, then the descriptor is a Page descriptor.
+		if ((l & (1 << 1)) == 0) {
+			struct BlockDescriptor block = {
+				.output_address = output_address,
+				.size = get_block_size(cfg, level),
+				.block = full_block,
+				.upper_attributes = full_block >> 47,
+				.lower_attributes = full_block & 0xfff,
+			};
+
+//			printf("%x %x\n", block.upper_attributes, block.lower_attributes);
+
+			// Block descriptor
+			// temporary hack to compute address in this descriptor
+			if (last_block != NULL) {
+				uint64_t last_block_end = last_block->output_address + last_block->size;
+
+//				printf("%lx vs %lx\n", last_block_end, block.output_address);
+
+				int blocks_align = last_block_end == block.output_address;
+				int attrs_same = (block.upper_attributes == last_block->upper_attributes)
+					&& (block.lower_attributes == last_block->lower_attributes);
+
+				if (blocks_align && attrs_same) {
+					if (cfg->current_combined_size == 0) {
+						cfg->initial_common_address = last_block->output_address;
+					}
+					cfg->current_combined_size += last_block->size + block.size;
+				} else {
+					if (cfg->current_combined_size != 0) {
+						printf("%sCombined blocks of size 0x%lx\n", get_indent_lvl(level), cfg->current_combined_size);
+						cfg->current_combined_size = 0;
+					}
+
+					printf("%sBlock descriptor -> 0x%lx\n", get_indent_lvl(level), output_address);
+					printf("%sDescriptor is at 0x%lx\n", get_indent_lvl(level), of);
+				}
 			}
 
-			last_addr = addr;
+			dummy_block = block;
+			last_block = &dummy_block;
 		} else {
-			// else, this is a table descriptor
-			uint32_t oa_addr_n = 0xa;
-			uint32_t oa_addr_mask = 0xffffffff;
-			uint32_t addr = l & (oa_addr_mask << oa_addr_n);
-
-			printf("Page descriptor pointing to 0x%x\n", addr);
-
-			int rc = walk_level(cfg, addr, level + 1); // Recurse
+			printf("Page descriptor pointing to 0x%lx\n", output_address);
+			int rc = walk_level(cfg, output_address, level + 1, last_block); // Recurse
 			if (rc) return rc;
 		}
 	}
@@ -147,7 +229,7 @@ int walk_level(struct ARMConfig *cfg, uint64_t of, int level) {
 }
 
 int walk_tt(struct ARMConfig *cfg) {
-	return walk_level(cfg, cfg->ttbr0_elx, 1);
+	return walk_level(cfg, cfg->ttbr0_elx, 1, NULL);
 }
 
 int main(void) {
