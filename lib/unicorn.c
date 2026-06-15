e8d0a783fb5e05f714248165f27390560940b9d4
Daniel C <brikbusters@gmail.com>
2026-06-15 14:40:18 -0400

    LE PPC error in unicorn startup

--- lib/unicorn.c
+++ lib/unicorn.c
@@ -117,6 +117,10 @@ int re_emulator(enum Arch arch, int opt, unsigned int base_addr, struct RetBuffe
 		if (opt & RET_BITS_64) _uc_mode |= UC_MODE_PPC64;
 		else if (opt & RET_BITS_32) _uc_mode |= UC_MODE_PPC32;
 		else _uc_mode |= UC_MODE_PPC64;
+		if (!(_uc_mode & UC_MODE_BIG_ENDIAN)) {
+			buffer_appendf(log, "Little Endian PowerPC is not supported in Unicorn Engine\n");
+			return -1;
+		}
 	} else {
 		buffer_appendf(log, "Unknown architecture\n");
 		return -1;
